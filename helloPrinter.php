a881e684cce9adb237f832a76281b059401cea03
williamhaynes <w.haynes@rgu.ac.uk>
2016-10-17 13:19:36 +0100

    Initial commit

---
+++ helloPrinter.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World 2.0</title>
+</head>
+<body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hype_
+ * Date: 17/10/2016
+ * Time: 13:13
+ */
+    $planet = $_GET["planet"];
+    echo "<p>Hello " . $planet . "<p>";
+?>
+</body>
+</html>
---
+++ helloSelector.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World 2.0</title>
+</head>
+    <body>
+    <h1>Where would you like to say hello</h1>
+    <ul>
+        <a href="helloPrinter.php?planet=Earth">Earth</a>
+        <a href="helloPrinter.php?planet=Mars">Mars</a>
+        <a href="helloPrinter.php?planet=Uranus">Uranus</a>
+    </ul>
+    <?php
+    /**
+    * Created by PhpStorm.
+    * User: hype_
+    * Date: 17/10/2016
+    * Time: 13:09
+    */?>
+    </body>
+
+</html>
